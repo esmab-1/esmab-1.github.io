af1b5491c698a7910ff1bdc1abb9cf53a8c1bd79
piccoag <150273569+piccoag@users.noreply.github.com>
2026-05-01 21:14:38 -0300

    cambio banner

--- script.js
+++ script.js
@@ -39,10 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbar');
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
-            navbar.style.padding = '0.5rem 0';
             navbar.style.boxShadow = '0 4px 10px rgba(0,0,0,0.1)';
         } else {
-            navbar.style.padding = '1rem 0';
             navbar.style.boxShadow = '0 2px 4px rgba(9, 30, 66, 0.08)';
         }
     });
